0b82a6e0affe05d64e7bbbee33d68a0c7909c7ec
naman-kc <57825499+naman-kc@users.noreply.github.com>
2020-07-08 21:27:14 +0530

    Add files via upload

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private float speed = 10f;
+
+	private Rigidbody2D myBody;
+
+	void Awake () {
+		myBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 vel = myBody.velocity;
+		vel.x = Input.GetAxis ("Horizontal") * speed;
+		myBody.velocity = vel;
+	}
+
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
